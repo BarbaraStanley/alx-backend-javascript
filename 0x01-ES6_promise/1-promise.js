fdb2de94ac6d4694f69fcf1e2a31dc0b780b8769
Barbara <barbeaboo@gmail.com>
2024-03-06 22:49:33 +0000

    2

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getFullResponseFromAPI(success) {
+	return new Promise((resolve, reject) => {
+		if (success == false) {
+			reject('The fake API is not working currently');
+		} else {
+			const info = { 'status':'200', 'body':'Success' };
+			resolve(info);
+		}
+	});
+}
