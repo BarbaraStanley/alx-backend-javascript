4c6878f54d408d5c2e4c6ace7355b37286e16e3b
Barbara <barbeaboo@gmail.com>
2024-03-06 23:04:15 +0000

    lint issues fixed

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,5 @@
 export default function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {
-		const v = 1;
-		if (v < 1) {
-			reject('rejected');
-		} else {
-			resolve('resolved');
-		}
-	});
+  return new Promise((resolve) => {
+    resolve('resolved');
+  });
 }
