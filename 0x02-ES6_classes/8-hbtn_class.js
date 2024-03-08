c48b55d8b396de4338c2808ffb58c33844115aa4
Barbara <barbeaboo@gmail.com>
2024-03-08 02:33:01 +0000

    8

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,14 @@
+export default Class HolbertonClass {
+  constructor(size, location) {
+    size = this.size;
+    location = this.location;
+  }
+
+  valueOf() {
+    return this.size;
+  }
+
+  toString() {
+    return this.location;
+  }
+}
