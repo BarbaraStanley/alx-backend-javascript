8c2ea381f107e915e7c1e1399f7890a453a0849b
Barbara <barbeaboo@gmail.com>
2024-03-10 10:35:58 +0000

    correct typo

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,7 +3,7 @@ export default function getFullResponseFromAPI(success) {
     if (success == false) {
       reject('The fake API is not working currently');
     } else {
-      const info = { status: '200', body: 'Success' };
+      const info = { status: 200, body: 'Success' };
       resolve(info);
     }
   });
